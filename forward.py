1ef1b20ca5c0592fe34e476a63dfb9a7f7636dd8
Sanchez0823 <arturosanchezavocado23@gmail.com>
2025-11-06 13:54:16 -0600

    Added more libraries and a description to forward.py

--- forward.py
+++ forward.py
@@ -1,3 +1,18 @@
 import numpy as np
-# a Raspberry Pi library might be needed.
+import time
+import RPi.GPIO as GPIO
+
+
+# Some libraries might still be needed.
+
+
 # Robot movement code starts here.
+
+"""
+The robot's movements will be guided by the trash it detects.
+When it sees trash, it will move towards it to collect it. Then, it will scan the environment.
+If no trash is detected, it will wander endlessly until it detects another trash object.
+It will repeat this process until the robot has spent at least one hour without collecting trash.
+That stage will be known as the "idle" stage, where the robot gets "bored" from not collecting trash for over 60 mins.
+If the robot's battery runs out, it will shut down until it is somewhat charged.
+"""
